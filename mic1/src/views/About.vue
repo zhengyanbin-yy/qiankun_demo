97e5d150de2ccb61fe86ffa330b43389fdf5c8de
郑彦彬 <iamzhengyanbin@163.com>
2022-01-24 16:06:18 +0800

    更新

--- mic1/src/views/About.vue
+++ mic1/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+      <button @click="$router.go(-1)">返回</button>
       <input v-model="val"/>
     <h3>我是  子应用1  中的页面</h3>
       <div>This is an about page</div>
--- mic1/src/views/function/manage.vue
+++ mic1/src/views/function/manage.vue
@@ -2,6 +2,7 @@
     <div>
         <button @click="$router.push('/list?isFormMicApp=yes')">跳转</button>
         <input type="text" v-model="test">
+        <div>我是子应用1 中的 manage 页面</div>
     </div>
 </template>
 
